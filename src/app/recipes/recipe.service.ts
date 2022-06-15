b6d8a49489c53ee7c4d6e8f5dcb18d180ebfa87c
nnvedward <nnvedward@gmail.com>
2022-06-16 00:45:15 +0100

    Passed ingredients from recipes to the shopping list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,7 +1,9 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>()
 
@@ -10,7 +12,13 @@ export class RecipeService {
         new Recipe('Another Test Recipe', 'A test recipe sample', 'https://picturetherecipe.com/wp-content/uploads/2020/07/Butter-Chicken-PictureTheRecipe.jpg', [new Ingredient('Fish', 1), new Ingredient('Spaghetti', 5)])
     ]
 
+    constructor(private slService: ShoppingListService) {}
+
     getRecipe() {
         return this.recipes.slice()
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients)
     }
 }
