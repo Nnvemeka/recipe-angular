fa364eda2a6cbc5d4074c99db4510fb894666cad
nnvedward <nnvedward@gmail.com>
2022-06-18 16:10:19 +0100

    Change the Event emitter observable to subject

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,11 @@
-import { EventEmitter, Injectable } from "@angular/core";
+import {  Injectable } from "@angular/core";
+import { Subject } from "rxjs";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
 @Injectable()
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>()
-
     private recipes: Recipe[] = [
         new Recipe('A Test Recipe', 'A test recipe sample', 'https://picturetherecipe.com/wp-content/uploads/2020/07/Butter-Chicken-PictureTheRecipe.jpg', [new Ingredient('Meat', 1), new Ingredient('Palm oil', 2)]),
         new Recipe('Another Test Recipe', 'A test recipe sample', 'https://picturetherecipe.com/wp-content/uploads/2020/07/Butter-Chicken-PictureTheRecipe.jpg', [new Ingredient('Fish', 1), new Ingredient('Spaghetti', 5)])
