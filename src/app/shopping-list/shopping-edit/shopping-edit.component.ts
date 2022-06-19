4dd7884352e92996470e599e462f6ac99bc20da1
nnvedward <nnvedward@gmail.com>
2022-06-20 00:07:15 +0100

    Setup delete function

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -47,6 +47,10 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.editMode = false
   }
 
+  onDelete() {
+    this.shoppingListService.deleteIngredient(this.editedItemIndex)
+    this.onClear()
+  }
   ngOnDestroy(): void {
     this.subscription.unsubscribe()
   }
