521269550fd3d00b8f1773e1ea1423d28d1345ac
nnvedward <nnvedward@gmail.com>
2022-06-22 00:38:28 +0100

    Resolved data before loading

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { map } from "rxjs";
+import { map, tap } from "rxjs";
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
 
@@ -21,18 +21,20 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        this.http
+        return this.http
             .get<Recipe[]>('https://prexcribe-6d944-default-rtdb.firebaseio.com/recipes.json')
-            .pipe(map(recipes => {
-                return recipes.map(recipe => {
-                    return {
-                        ...recipe,
-                        ingredients: recipe.ingredients ? recipe.ingredients : []
-                    }
+            .pipe(
+                map(recipes => {
+                    return recipes.map(recipe => {
+                        return {
+                            ...recipe,
+                            ingredients: recipe.ingredients ? recipe.ingredients : []
+                        }
+                    })
+                }),
+                tap(recipes => {
+                    this.recipeService.setRecipes(recipes)
                 })
-            }))
-            .subscribe(recipes => {
-                this.recipeService.setRecipes(recipes)
-            })
+            )
     }
 }
