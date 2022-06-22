a65ad5b87a77a5c69986bb71e99b340681c8d25e
nnvedward <nnvedward@gmail.com>
2022-06-22 13:07:17 +0100

    Created and stored user data

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { catchError, throwError } from "rxjs";
+import { catchError, Subject, tap, throwError } from "rxjs";
+import { User } from "./user.model";
 
 export interface AuthResponseData {
     idToken: string
@@ -13,6 +14,7 @@ export interface AuthResponseData {
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+    user = new Subject<User>()
 
     constructor(private http: HttpClient) { }
 
@@ -24,7 +26,9 @@ export class AuthService {
                     password: password,
                     returnSecureToken: true
                 }
-            ).pipe(catchError(this.handleError))
+            ).pipe(catchError(this.handleError), tap(resData => this.handleAuthentication(
+                resData.email, resData.localId, resData.idToken, +resData.expiresIn
+            )))
     }
 
     login(email: string, password: string) {
@@ -35,7 +39,15 @@ export class AuthService {
                 password: password,
                 returnSecureToken: true
             }
-        ).pipe(catchError(this.handleError))
+        ).pipe(catchError(this.handleError), tap(resData => this.handleAuthentication(
+            resData.email, resData.localId, resData.idToken, +resData.expiresIn
+        )))
+    }
+
+    private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
+        const expirationDate = new Date(new Date().getTime() + expiresIn * 1000)
+        const user = new User(email, userId, token, expirationDate)
+        this.user.next(user)
     }
 
     private handleError(errorRes: HttpErrorResponse) {
