b95969b0a4b9db5b5d4990a99de7b008ad6a1a01
nnvedward <nnvedward@gmail.com>
2022-06-25 14:37:40 +0100

    Managed authentication flow with ngrx

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,11 @@
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
+import { Store } from "@ngrx/store";
 import { BehaviorSubject, catchError, tap, throwError } from "rxjs";
 import { User } from "./user.model";
+import * as fromApp from '../store/app.reducer'
+import * as AuthActions from './store/auth.action'
 
 export interface AuthResponseData {
     idToken: string
@@ -15,10 +18,14 @@ export interface AuthResponseData {
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-    user = new BehaviorSubject<User>(null)
+    // user = new BehaviorSubject<User>(null)
     private tokenEpirationTimer: any
 
-    constructor(private http: HttpClient, private router: Router) { }
+    constructor(
+        private http: HttpClient,
+        private router: Router,
+        private store: Store<fromApp.AppState>
+    ) { }
 
     signUp(email: string, password: string) {
         return this.http
@@ -47,18 +54,32 @@ export class AuthService {
     }
 
     autoLogin() {
-        const userData: {email: string, id: string, _token: string, _tokenExpDate: string} = JSON.parse(localStorage.getItem('userData'))
-        const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpDate))
+        const userData: { email: string, id: string, _token: string, _tokenExpDate: string } = JSON.parse(localStorage.getItem('userData'))
+        const loadedUser = new User(
+            userData.email,
+            userData.id,
+            userData._token,
+            new Date(userData._tokenExpDate)
+        )
 
         if (loadedUser.token) {
-            this.user.next(loadedUser)
+            // this.user.next(loadedUser)
+            this.store.dispatch(
+                new AuthActions.Login({
+                    email: loadedUser.email,
+                    userId: loadedUser.id,
+                    token: loadedUser.token,
+                    expirationDate: new Date(userData._tokenExpDate)
+                })
+            )
             const expirationDate = new Date(userData._tokenExpDate).getTime() - new Date().getTime()
             this.autoLogout(expirationDate)
         }
     }
 
     logout() {
-        this.user.next(null)
+        // this.user.next(null)
+        this.store.dispatch(new AuthActions.Logout())
         this.router.navigate(['/auth'])
         localStorage.removeItem('userData')
         if (this.tokenEpirationTimer) {
@@ -76,7 +97,15 @@ export class AuthService {
     private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
         const expirationDate = new Date(new Date().getTime() + expiresIn * 1000)
         const user = new User(email, userId, token, expirationDate)
-        this.user.next(user)
+        // this.user.next(user)
+        this.store.dispatch(
+            new AuthActions.Login({
+                email: email,
+                userId: userId,
+                token: token,
+                expirationDate: expirationDate
+            })
+        )
         this.autoLogout(expiresIn * 1000)
         localStorage.setItem('userData', JSON.stringify(user))
     }
